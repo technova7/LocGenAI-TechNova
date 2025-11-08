f08cd73033f911e0cef06cfa1b9e55f5060ffb26
technova7 <technova7@yahoo.com>
2025-11-08 19:04:54 +0530

    Delete locgenai directory

--- locgenai/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.1.0"
-
-# Re-export useful functions for convenience:
-from .model_wrapper import get_response, find_local_answer  # noqa: F401
-
-__all__ = ["__version__", "get_response", "find_local_answer"]
