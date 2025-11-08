4f4c891bae9d6c10d85f7be1736e9611d4b53e40
technova7 <technova7@yahoo.com>
2025-11-08 19:01:50 +0530

    Update __init__.py

--- locgenai/__init__.py
+++ locgenai/__init__.py
@@ -1,3 +1,6 @@
-# locgenai package initializer
-# makes 'locgenai' importable as a Python module
-__all__ = ["model_wrapper"]
+__version__ = "0.1.0"
+
+# Re-export useful functions for convenience:
+from .model_wrapper import get_response, find_local_answer  # noqa: F401
+
+__all__ = ["__version__", "get_response", "find_local_answer"]
